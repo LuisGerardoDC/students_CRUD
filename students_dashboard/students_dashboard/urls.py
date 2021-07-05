aacdbccdecad087db6ceec88d179bb308b58aedb
Luis Hernandez <luisgerarhern@outlook.com>
2021-07-05 12:58:47 -0500

    views agregados

--- students_dashboard/students_dashboard/urls.py
+++ students_dashboard/students_dashboard/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from users import views as users_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', users_views.login),
+    path('sign_up/',users_views.sign_up)
 ]
--- students_dashboard/users/views.py
+++ students_dashboard/users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    """ log in screen"""
+    return render(request,'login.html')
+
+def sign_up(request):
+    """ log in screen"""
+    return render(request,'sign_up.html')
